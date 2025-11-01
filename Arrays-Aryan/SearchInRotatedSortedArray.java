150a6382561ab951ac62c1400369e11d6822bde0
AryanMule <mulearyan2@gmail.com>
2025-11-01 09:01:18 +0530

    search in rotated and sorted array

---
+++ Arrays-Aryan/SearchInRotatedSortedArray.java
@@ -0,0 +1,36 @@
+
+public class SearchInRotatedSortedArray {
+
+    public static int search(int[] arr, int target) {
+        int n = arr.length;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            //left half is sorted
+            if (arr[left] <= arr[mid]) {
+                if (target < arr[mid] && target >= arr[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } //right hald is sorted
+            else {
+                if (target > arr[mid] && target <= arr[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        System.out.println("Index of target is: " + search(arr, target));
+    }
+}
