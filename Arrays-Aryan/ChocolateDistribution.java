24cbb4546a93b72d1be8547660df9f4f8c1a305d
AryanMule <mulearyan2@gmail.com>
2025-11-02 09:12:37 +0530

    chocolate distribution array

---
+++ Arrays-Aryan/ChocolateDistribution.java
@@ -0,0 +1,33 @@
+
+import java.util.Arrays;
+
+public class ChocolateDistribution {
+
+    public static int findMinDiff(int[] arr, int m) {
+        if (m == 0 || arr.length == 0) {
+            return 0;
+        }
+        if (arr.length < m) {
+            return -1;
+        }
+        Arrays.sort(arr);
+        int minDiff = Integer.MAX_VALUE;
+        for (int i = 0; i + m - 1 < arr.length; i++) { // SLIDING WINDOR OF SIZE M 
+            int diff = arr[i + m - 1] - arr[i];
+            if (diff < minDiff) {
+                minDiff = diff;
+            }
+        }
+        return minDiff;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 3, 2, 4, 9, 12, 56};
+        int m = 3;
+        System.out.println(findMinDiff(arr, m)); // Output: 2
+
+        m = 5;
+        System.out.println(findMinDiff(arr, m)); // Output: 7
+    }
+}
